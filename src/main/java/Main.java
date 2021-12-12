0913bed8d83f2ff8074265c6116a7846cf27882e
Geo1981 <georgii81@gmail.com>
2021-12-12 20:06:14 +0300

    JavaHomeWork4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,27 @@
+import java.util.List;
+
 public class Main {
-    2
+    public static void main(String[] args) {
+
+    }
+
+    public int search(Integer n, Integer[] list){
+
+    }
+
+    public String reverse(String s) {
+
+    }
+
+    public Integer maximum(Integer[] list){
+
+    }
+
+    public Double average(List<Integer> list){
+
+    }
+
+    public List<String> search(List<String> list){
+
+    }
 }
